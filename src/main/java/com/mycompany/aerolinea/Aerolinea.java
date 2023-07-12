f6b255849882788b7a5f30ab3061ba9a66ce81e0
Picos <brayan.garcia232168@potros.itson.edu.mx>
2023-07-12 01:10:28 -0600

    1er Commit

---
+++ src/main/java/com/mycompany/aerolinea/Aerolinea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aerolinea;
+
+/**
+ *
+ * @author PC
+ */
+public class Aerolinea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
